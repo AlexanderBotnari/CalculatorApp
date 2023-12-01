927d62c999a2a4a3e0d904e43f5d66c636cc91f2
AlexanderBotnari <sandubotnari99@gmail.com>
2023-12-01 22:14:35 +0200

    add percent, negative/positive number and decimal numbers bug fixes

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -6,9 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editTextInputNumber;
-    private double operand1 = Double.NaN;
-    private String pendingOperation = "=";
-    private boolean isDecimal = false;
+    private String operator = "";
+    private String oldNumber;
+    private boolean isNew = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,67 +29,136 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button8).setOnClickListener(v -> numberClicked("8"));
         findViewById(R.id.button9).setOnClickListener(v -> numberClicked("9"));
         findViewById(R.id.pointButton).setOnClickListener(v -> numberClicked("."));
+        findViewById(R.id.plusMinusButton).setOnClickListener(v -> numberClicked("-"));
 
         // Operation buttons
         findViewById(R.id.plusButton).setOnClickListener(v -> operationClicked("+"));
         findViewById(R.id.minusButton).setOnClickListener(v -> operationClicked("-"));
         findViewById(R.id.multiplyButton).setOnClickListener(v -> operationClicked("*"));
         findViewById(R.id.divideButton).setOnClickListener(v -> operationClicked("/"));
-        findViewById(R.id.equalButton).setOnClickListener(v -> operationClicked("="));
+        findViewById(R.id.equalButton).setOnClickListener(v -> equalClick());
+        findViewById(R.id.percentButton).setOnClickListener(v -> percentClick());
         findViewById(R.id.clearButton).setOnClickListener(v -> clearClicked());
     }
 
+    private void percentClick() {
+        if (operator.equals("")){
+            String number = editTextInputNumber.getText().toString();
+            double temp = Double.parseDouble(number) / 100;
+            number = temp+"";
+            editTextInputNumber.setText(number);
+        }else{
+            String newNumber = editTextInputNumber.getText().toString();
+            Double result = 0.0;
+
+            switch (operator){
+                case "+":
+                    result = Double.parseDouble(oldNumber) + Double.parseDouble(oldNumber) * Double.parseDouble(newNumber) / 100;
+                    break;
+                case "-":
+                    result = Double.parseDouble(oldNumber) - Double.parseDouble(oldNumber) * Double.parseDouble(newNumber) / 100;
+                    break;
+                case "*":
+                    result = Double.parseDouble(oldNumber) * Double.parseDouble(newNumber) / 100;
+                    break;
+                case "/":
+                    result = Double.parseDouble(oldNumber) / Double.parseDouble(newNumber) * 100;
+                    break;
+            }
+
+            editTextInputNumber.setText(String.valueOf(result));
+            operator = "";
+        }
+    }
+
     private void numberClicked(String number) {
+        if(isNew)
+            editTextInputNumber.setText("");
+        isNew = false;
         String currentText = editTextInputNumber.getText().toString();
-        editTextInputNumber.setText(number);
+
+        switch (number){
+            case "1": currentText = currentText + "1"; break;
+            case "2": currentText = currentText + "2"; break;
+            case "3": currentText = currentText + "3"; break;
+            case "4": currentText = currentText + "4"; break;
+            case "5": currentText = currentText + "5"; break;
+            case "6": currentText = currentText + "6"; break;
+            case "7": currentText = currentText + "7"; break;
+            case "8": currentText = currentText + "8"; break;
+            case "9": currentText = currentText + "9"; break;
+            case "0": currentText = currentText + "0"; break;
+
+            case ".":
+                if (!currentText.contains(".")){
+                    currentText = currentText + ".";
+                }
+                break;
+
+            case "-":
+                if (currentText.equals("0") || currentText.equals("") || currentText.equals("0.0")){
+                    currentText = "0";
+                }else{
+                    if (currentText.charAt(0) == '-'){
+                        currentText = currentText.substring(1);
+                    }else{
+                        currentText = "-" + currentText;
+                    }
+                }
+                break;
+        }
+
+        editTextInputNumber.setText(currentText);
+
     }
 
     private void operationClicked(String operation) {
-        String value = editTextInputNumber.getText().toString();
+        isNew = true;
+        oldNumber = editTextInputNumber.getText().toString();
 
-        if (!value.isEmpty()) {
-            double currentValue = Double.parseDouble(value);
-
-            if (!Double.isNaN(operand1)) {
-                switch (pendingOperation) {
-                    case "=":
-                        operand1 = currentValue;
-                        break;
+                switch (operation) {
                     case "+":
-                        operand1 += currentValue;
+                        operator = "+";
                         break;
                     case "-":
-                        operand1 -= currentValue;
+                        operator = "-";
                         break;
                     case "*":
-                        operand1 *= currentValue;
+                        operator = "*";
                         break;
                     case "/":
-                        if (currentValue != 0) {
-                            operand1 /= currentValue;
-                        } else {
-                            editTextInputNumber.setText("Error");
-                            return;
-                        }
+                        operator = "/";
+                        break;
+                    case "%":
+                        operator = "%";
                         break;
                 }
-                editTextInputNumber.setText(String.valueOf(operand1));
-            } else {
-                operand1 = currentValue;
-            }
-        }
-
-        pendingOperation = operation.equals("=") ? "=" : operation;
     }
 
-//    private void clearClicked() {
-//        operand1 = Double.NaN;
-//        pendingOperation = "=";
-//        editTextInputNumber.setText("");
-//    }
+    private void equalClick(){
+        String newNumber = editTextInputNumber.getText().toString();
+        Double result = 0.0;
+
+        switch (operator){
+            case "+":
+                result = Double.parseDouble(oldNumber) + Double.parseDouble(newNumber);
+                break;
+            case "-":
+                result = Double.parseDouble(oldNumber) - Double.parseDouble(newNumber);
+                break;
+            case "*":
+                result = Double.parseDouble(oldNumber) * Double.parseDouble(newNumber);
+                break;
+            case "/":
+                result = Double.parseDouble(oldNumber) / Double.parseDouble(newNumber);
+                break;
+        }
+
+        editTextInputNumber.setText(String.valueOf(result));
+    }
+
     private void clearClicked() {
-        operand1 = Double.NaN;
-        pendingOperation = "=";
-        editTextInputNumber.setText("");
+        editTextInputNumber.setText("0");
+        isNew = true;
     }
 }
